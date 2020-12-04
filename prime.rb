08a314063f52a3e71c30e41fd8269b46cdb8eb5a
Keith Filkins <kafilkins@me.com>
2020-12-04 22:13:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
-def prime?(n)
-if (2..n-1).none? { |divisor| n % divisor == 0}
-  true
-else
-  false
-end
-end
+def is_prime(x):
+    if x < 2:
+        return False
+    else:
+        for n in range(2,x):
+            if x % n == 0:
+               return False
+        return True
