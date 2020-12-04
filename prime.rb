e0b2d02b4962bd8dc5f877499eb56ac555385415
Keith Filkins <kafilkins@me.com>
2020-12-04 22:13:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
-def is_prime(x):
-    if x < 2:
+def is_prime(x)
+    if x < 2
         return False
-    else:
+    else
         for n in range(2,x):
             if x % n == 0:
                return False
